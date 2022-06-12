2865745f37c1bd690b2dcd879c0d2eeff4342ba3
Akhilpb15 <104380444+Akhilpb15@users.noreply.github.com>
2022-06-12 23:22:03 +0530

    Add files via upload

---
+++ Assn4 12 june output.py
@@ -0,0 +1,33 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\asus\OneDrive\Desktop\PP\Assn.4 12 June.py =========
+Q1
+Enter mark:79
+Grade: B
+Q2
+Enter year:2002
+Not a leap year
+Q3
+Question 1:2x1=2
+Right!
+Question 2:0x7=0
+Right!
+Question 3:8x0=8
+Wrong. The correct answer is 0
+Question 4:4x7=28
+Right!
+Question 5:2x3=66
+Wrong. The correct answer is 6
+Question 6:1x8=22
+Wrong. The correct answer is 8
+Question 7:7x9=63
+Right!
+Question 8:1x1=1
+Right!
+Question 9:4x6=24
+Right!
+Question 10:3x3=6
+Wrong. The correct answer is 9
+Q4
+Answer: 177
